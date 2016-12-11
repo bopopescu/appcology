0ab2742420f1bff308dcda4c8c5a48fd269a1ce2
moi235 <moiabadi@hotmail.com>
2016-12-11 17:54:52 -0500

    d

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from flask import Flask, request, jsonify, g, Response
 from helpers import send_response
 from receive import Receiver
@@ -5,6 +6,17 @@ from send import Message
 import json
 import requests
 
+'''
+to deploy: 
+git add .
+git commit -m "message"
+git push heroku master
+'''
+'''
+to test before you deploy
+source app/bin/activate (do only once in each terminal session)
+python application.py
+'''
 
 GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
 
@@ -19,17 +31,22 @@ def response_handler(request):
 	    user_id = receiver.get_sender_messenger_id()
 
 	    # Text from user
-	    text = receiver.get_text(hi)
+	    text = receiver.get_text()
 
 	    # Send message back
-	    Message(text, user_id).send(hi)
+	    Message(text, user_id).send()
     	
-	except:
+	except e:
+		print(e, file=sys.stderr)
 		pass
 
 	return send_response()
 
 
 # Sentences we'll respond with if the user greeted us
-
+def check_for_greeting(sentence):
+    """If any of the words in the user's input was a greeting, return a greeting response"""
+    for word in sentence.words:
+        if word.lower() in GREETING_KEYWORDS:
+            return random.choice(GREETING_RESPONSES)
 
