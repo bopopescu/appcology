0e84dd802e68d641dbe3ef8c7b8171f90d4e81ab
moi235 <moiabadi@hotmail.com>
2016-12-15 19:01:43 -0500

    playing with facepy

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from flask import Flask, request, jsonify, g, Response
 from helpers import send_response
 from receive import Receiver
-from send import Message, ACCESS_TOKEN
+from send import Message
 import json
 import requests
 import random
@@ -33,6 +33,7 @@ SEPERATE TERMINAL WINDOWS
 GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's",)
 GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
 
+ACCESS_TOKEN = 'EAAZAZC6jHx2vcBAEIwv2jUBIcXjlr2UgVvKHPJ9QgsWIhEggalW1K96YAuMXkKQMux9zjsWQfPUXWVsGK0ooWvGlwsZCoZAZClfpE6sv5Ntg9ECiJfruphS7gc9DQ1hy5DuGM9juzf7fb0eZCbUiHhhXrpJUELsNQucHOivKoLDgZDZD'
 graph = GraphAPI(ACCESS_TOKEN)
 
 def response_handler(request):
@@ -70,8 +71,10 @@ def check_for_greeting(sentence):
 
 def get_facebook_data():
 	events = graph.get('me/events')
-	data = []
-	for event in events['data']:
-		data.append(event['name'])
+	# data = []
+	# for event in events['data']:
+	# 	data.append(event['name'])
+	firstEvent = events['data'][0]['name']
+	# event = graph.get_objects(ids=157056878108860, fields='attending_count,declined_count')
 
-	return "Here are Emory's events: " + data
+	return "Here is your first Emory events: " + firstEvent
